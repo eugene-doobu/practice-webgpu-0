6d2bb2d2cd9ea8e620e207d6bf97d0c2ea74fafc
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-01-02 22:54:54 +0900

    Revert "카메라 이동 연산 수정"
    This reverts commit 4874a3710741a5321713b84a046175a9935ad113.

--- src/engine/camera.ts
+++ src/engine/camera.ts
@@ -1,4 +1,4 @@
-import {glMatrix, mat4, quat, vec3} from 'gl-matrix';
+import { glMatrix, mat4, vec3 } from 'gl-matrix';
 import CameraSetting, { canvasSize } from "../setting";
 
 export default class Camera{
@@ -64,13 +64,12 @@ export default class Camera{
     }
 
     public moveForward(distance: number): void {
-        let invertedView = mat4.create();
-        mat4.invert(invertedView, this.viewMatrix);
-
-        const sliceValues = invertedView.slice(8, 11);
-        let forward = vec3.fromValues(sliceValues[0], sliceValues[1], sliceValues[2]);
-        vec3.normalize(forward, forward);
-        vec3.scale(forward, forward, distance);
+        const forward = vec3.create();
+        vec3.set(forward, 0, 0, distance);
+        const toRadian = glMatrix.toRadian;
+        vec3.rotateX(forward, forward, [0, 0, 0], toRadian(this.rotation[0]));
+        vec3.rotateY(forward, forward, [0, 0, 0], toRadian(this.rotation[1]));
+        vec3.rotateZ(forward, forward, [0, 0, 0], toRadian(this.rotation[2]));
         vec3.add(this.position, this.position, forward);
 
         this.clampPosition(this.position);
@@ -78,13 +77,12 @@ export default class Camera{
     }
 
     public moveRight(distance: number): void {
-        let invertedView = mat4.create();
-        mat4.invert(invertedView, this.viewMatrix);
-
-        const sliceValues = invertedView.slice(0, 3);
-        let right = vec3.fromValues(sliceValues[0], sliceValues[1], sliceValues[2]);
-        vec3.normalize(right, right);
-        vec3.scale(right, right, distance);
+        const right = vec3.create();
+        vec3.set(right, distance, 0, 0);
+        const toRadian = glMatrix.toRadian;
+        vec3.rotateX(right, right, [0, 0, 0], toRadian(this.rotation[0]));
+        vec3.rotateY(right, right, [0, 0, 0], toRadian(this.rotation[1]));
+        vec3.rotateZ(right, right, [0, 0, 0], toRadian(this.rotation[2]));
         vec3.add(this.position, this.position, right);
 
         this.clampPosition(this.position);
@@ -92,13 +90,12 @@ export default class Camera{
     }
 
     public moveUp(distance: number): void {
-        let invertedView = mat4.create();
-        mat4.invert(invertedView, this.viewMatrix);
-
-        const sliceValues = invertedView.slice(4, 7);
-        let up = vec3.fromValues(sliceValues[0], sliceValues[1], sliceValues[2]);
-        vec3.normalize(up, up);
-        vec3.scale(up, up, distance);
+        const up = vec3.create();
+        vec3.set(up, 0, distance, 0);
+        const toRadian = glMatrix.toRadian;
+        vec3.rotateX(up, up, [0, 0, 0], toRadian(this.rotation[0]));
+        vec3.rotateY(up, up, [0, 0, 0], toRadian(this.rotation[1]));
+        vec3.rotateZ(up, up, [0, 0, 0], toRadian(this.rotation[2]));
         vec3.add(this.position, this.position, up);
 
         this.clampPosition(this.position);
@@ -113,15 +110,6 @@ export default class Camera{
         position[2] = Math.min(position[2], this.maxDistance);
         position[2] = Math.max(position[2], -this.maxDistance);
         return position;
-    }
-
-    private getRotationQuat(): quat {
-        let q = quat.create();
-        quat.fromEuler(q, this.rotation[0], this.rotation[1], this.rotation[2]);
-        quat.rotateX(q, q, this.rotation[0] * 180 / Math.PI);
-        quat.rotateY(q, q, this.rotation[1] * 180 / Math.PI);
-        quat.rotateZ(q, q, this.rotation[2] * 180 / Math.PI);
-        return q;
     }
 
     public rotateXY(x: number, y: number): void {
