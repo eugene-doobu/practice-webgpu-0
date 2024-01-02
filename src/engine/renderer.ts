c6986c1bdd141eae6dfe3cc40fa18e35f80e054c
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-01-02 20:32:46 +0900

    resizeBackings 메서드 추가

--- src/engine/renderer.ts
+++ src/engine/renderer.ts
@@ -1,11 +1,27 @@
 export default class Renderer{
-    canvas: HTMLCanvasElement;
+    private canvas: HTMLCanvasElement;
+
+    // web-gpu entry
+    private device: GPUDevice;
+    private queue: GPUQueue;
+
+    // frame backings
+    private context: GPUCanvasContext;
+    private colorTexture: GPUTexture;
+    private colorTextureView: GPUTextureView;
+    private depthTexture: GPUTexture;
+    private depthTextureView: GPUTextureView;
 
     constructor(canvas: HTMLCanvasElement){
         this.canvas = canvas;
     }
 
-    async init(): Promise<boolean>{
+    public async Start() {
+        if (!await this.init()) return;
+        this.resizeBackings();
+    }
+
+    private async init(): Promise<boolean>{
         // 현재 브라우징 컨텍스트에 대한 GPU 객체를 반환, 이는 WebGPU API의 진입점
         const entry: GPU = navigator.gpu;
         if (!entry) {
@@ -19,7 +35,31 @@ export default class Renderer{
             return false;
         }
 
-        const device: GPUDevice = await adapter.requestDevice();
-        const queue: GPUQueue = device.queue;
+        this.device = await adapter.requestDevice();
+        this.queue = this.device.queue;
+    }
+
+    private resizeBackings(){
+        this.context = this.canvas.getContext('webgpu');
+        const canvasConfig: GPUCanvasConfiguration = {
+            device: this.device,
+            format: navigator.gpu.getPreferredCanvasFormat(),
+            alphaMode: "opaque",
+            usage: GPUTextureUsage.RENDER_ATTACHMENT
+        };
+        this.context.configure(canvasConfig);
+
+        this.colorTexture = this.context.getCurrentTexture();
+        this.colorTextureView = this.colorTexture.createView();
+
+        const depthTextureDesc: GPUTextureDescriptor = {
+            size: [this.canvas.width, this.canvas.height, 1],
+            dimension: '2d',
+            format: 'depth24plus',
+            usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC
+        };
+
+        this.depthTexture = this.device.createTexture(depthTextureDesc);
+        this.depthTextureView = this.depthTexture.createView();
     }
 }
