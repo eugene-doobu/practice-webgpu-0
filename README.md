7bf56588e2d80fb1a1130ac42d2762fb457040c6
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-04-13 02:00:14 +0900

    reset repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# practice-webgpu-0
-webgpu 기초 강의 0
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import Renderer from "./engine/renderer";
-
-const canvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
-canvas.width = canvas.height = 800;
-
-const renderer = new Renderer(canvas);
-renderer.Start();
