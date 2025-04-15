aaa3efb0d5a24a848e6264eb69f29ee8498cf041
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-04-15 10:58:55 +0900

    Create 002_Comments.rs

---
+++ easy_rust/002_Comments.rs
@@ -0,0 +1,10 @@
+/// docs
+// struct Book; // warning 발생
+
+fn main() {
+    // this is comment
+    println!("Hello, world!");
+
+    // 사용하지 않는 변수 warning 제거 => _로시작하는 변수
+    let _some_number /* 사이 주석 */ : i16 = 10;
+}
